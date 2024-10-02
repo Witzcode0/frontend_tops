adbb58f1b2b64caec3f19aca87dbdd428a443393
Witzcode0 <brijesh.gondaliya07@gmail.com>
2024-10-02 18:20:32 +0530

    componentDidMount, unmount and update

--- ReactJS/react-components/src/App.js
+++ ReactJS/react-components/src/App.js
@@ -4,24 +4,10 @@ import logo from './logo.svg';
 import './App.css';
 import GreetingByFunction from './Components/Functions/Greeting';
 import GreetingByClass from './Components/Class/Greeting';
-import IncDecQty from './Components/Rstate/Cstate';
+import { IncDecQty, ShowAge } from './Components/Rstate/Cstate';
 
 // Class base first component
 class App extends Component {
-
-  constructor(){
-    super();
-    this.state ={
-      age:0  
-    }
-  }
-
-  myAge(){
-    return this.setState({
-      age: 27
-    })
-  }
-
   componentDidMount(){
     console.log("componentDidMount : When component render first time.")
   }
@@ -30,10 +16,7 @@ class App extends Component {
     return (
       <div className="App">
         <GreetingByFunction name="Brijesh Gondaliya" />
-        <h1>My age is: {this.state.age}</h1>
-        {/* <button onClick={this.myAge.bind(this)}>Know my age?</button> */}
-
-        <button onClick={() => {this.myAge()}}>Know my age?</button>
+        <ShowAge />
         <GreetingByClass company="Tops Technologies Pvt. Ltd." position="Sr. Technical Trainers" />
         <IncDecQty />
       </div>
--- ReactJS/react-components/src/Components/Rstate/Cstate.js
+++ ReactJS/react-components/src/Components/Rstate/Cstate.js
@@ -1,34 +1,76 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState, Component } from "react";
 
 const IncDecQty = () => {
+  const [qty, setQty] = useState(0);
 
-    // syntax: const [state, setState] = useState(initialState)
-    const [qty, setQty] = useState(0);
+  function incrementQty() {
+    return setQty(qty + 1);
+  }
 
-    function incrementQty(){
-       return setQty(qty+1);
+  function decrementQty() {
+    return setQty(qty - 1);
+  }
+
+  useEffect(
+    () => {
+      if (qty < 0) {
+        alert("Quantity cannot be negative!");
+        setQty(0);
+      }
+    },
+    [qty]
+  );
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <button onClick={incrementQty}>+</button>
+      <h1 style={{ margin: "0px 10px" }}>
+        {qty}
+      </h1>
+      <button onClick={decrementQty}>-</button>
+    </div>
+  );
+};
+
+class ShowAge extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.age !== this.state.age) {
+      console.log(
+        `componentDidUpdate : The age has been updated to: ${this.state.age}`
+      );
     }
+  }
 
-    function decrementQty(){
-        return setQty(qty-1);
-    }
+  componentWillUnmount() {
+    console.log("componentWillUnmount : Component is about to be unmounted.");
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: null
+    };
+  }
 
-    // syntax:  useEffect(setup, dependencies?)
-    useEffect( () => {
-        if (qty < 0){
-            alert('Quantity cannot be negative!');
-            setQty(0); // Reset to zero if qty becomes negative
-        }
-    }, [qty])
+  myAge = () => {
+    this.setState({ age: 25 });
+  };
 
+  render() {
     return (
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <button onClick={incrementQty}>+</button>
-            <h1 style={{margin: "0px 10px"}}>{qty}</h1>
-            <button onClick={decrementQty}>-</button>
-
-        </div>
-    )
+      <div>
+        <h1>
+          My age is: {this.state.age !== null ? this.state.age : "Unknown"}
+        </h1>
+        <button onClick={this.myAge}>Know my age?</button>
+      </div>
+    );
+  }
 }
 
-export default IncDecQty
+export { IncDecQty, ShowAge };
